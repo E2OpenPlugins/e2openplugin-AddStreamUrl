173f44e2d9d61b2fdffecac38ccd2aa7483aa49f
persianpros <persianpros@yahoo.com>
2021-04-14 15:32:32 +0430

    PEP8 double aggressive E20 and E211

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 pkg = 'Extensions.AddStreamUrl'
-setup (name='enigma2-plugin-extensions-addstreamurl',
+setup(name='enigma2-plugin-extensions-addstreamurl',
 	version='1.0',
 	description='Add a stream url to channel list',
 	packages=[pkg],
